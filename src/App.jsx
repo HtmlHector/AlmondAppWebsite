c46ce0e9c76ab880c68934524992996bd78adc28
Hector <trashdev098@gmail.com>
2025-10-22 10:58:15 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,19 @@ import LandingPage from "./pages/LandingPage.jsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import UpdatesPage from "./pages/UpdatesPage.jsx";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, HashRouter } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      {/*<BrowserRouter>*/}
-      {/*  <Routes>*/}
-      {/*    <Route path="/" element={<LandingPage />} />*/}
-      {/*    <Route path="/join" element={<UpdatesPage />} />*/}
-      {/*  </Routes>*/}
-      {/*</BrowserRouter>*/}
-      <h1>Hello</h1>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/join" element={<UpdatesPage />} />
+        </Routes>
+      </HashRouter>
     </>
   );
 }
