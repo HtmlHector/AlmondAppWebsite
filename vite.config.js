0d2bfe5afda0b87862a9d3e907400a9000b8d09d
Hector <trashdev098@gmail.com>
2025-10-22 09:04:48 -0400

    Changhed

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/AlmondAppWebsite/", // <-- add this
-
   plugins: [react(), tailwindcss()],
 });
