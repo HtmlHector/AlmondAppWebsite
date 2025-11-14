70f8432c0dd2afdd22588ff974a425f174cfab70
ryancortenbach <cortenbach.ryan.23@gmail.com>
2025-11-14 12:14:23 -0500

    Add 404 redirect support for GitHub Pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,36 @@ import SignUpPage from "./pages/SignUpPage.jsx";
 import OnboardingFlow from "./pages/OnboardingFlow.jsx";
 import PrivacyPolicy from "./pages/PrivacyPolicy.jsx";
 import TermsOfService from "./pages/TermsOfService.jsx";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+function AppRoutes() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const redirectPath = sessionStorage.getItem('redirectPath');
+    if (redirectPath) {
+      sessionStorage.removeItem('redirectPath');
+      navigate(redirectPath);
+    }
+  }, [navigate]);
+
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/join" element={<UpdatesPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/get-started" element={<OnboardingFlow />} />
+      <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+      <Route path="/termsofservice" element={<TermsOfService />} />
+    </Routes>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/join" element={<UpdatesPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/get-started" element={<OnboardingFlow />} />
-        <Route path="/privacypolicy" element={<PrivacyPolicy />} />
-        <Route path="/termsofservice" element={<TermsOfService />} />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
